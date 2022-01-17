16fab257528f6bfd3713588aef18c818c972503c
Oluwadamilola Adigun <ovadigun@sidmach.com>
2022-01-17 12:36:47 +0100

    returned alembic  revision files

---
+++ alembic/versions/5c460f21cf07_recreate_created_at_for_post_table.py
@@ -0,0 +1,27 @@
+"""recreate created at for post table
+
+Revision ID: 5c460f21cf07
+Revises: dd5642e3d71e
+Create Date: 2022-01-11 15:50:52.080327
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c460f21cf07'
+down_revision = 'dd5642e3d71e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():    
+    op.add_column('posts', sa.Column('date_created', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False )),
+    pass
+
+
+def downgrade():
+    op.drop_column('posts', 'created_at')
+    pass
+
